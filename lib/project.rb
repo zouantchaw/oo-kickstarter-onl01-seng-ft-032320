b59f53d80b66e95e44e91954648f91b850e5fc72
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 17:28:09 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project 
+  attr_reader :title, :backers
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self 
+  end
+  
+
+end 
